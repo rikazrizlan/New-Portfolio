1eb10663861a8fbcb403f45db9223877361152db
Rikaz Rizlan <rikzrikaz@gmail.com>
2020-11-06 07:43:17 +0530

    disabled dark mode button

---
+++ App.js
@@ -0,0 +1,21 @@
+/*
+const modeBtn = document.querySelector('.mode-btn');
+
+modeBtn.addEventListener('click', () =>{
+    const grandParent  = modeBtn.parentElement.parentElement;
+    grandParent.classList.toggle('dark-mode');
+    if(grandParent.classList.contains("dark-mode")) {
+        modeBtn.innerText = "Lignt Mode";
+    } else {
+        modeBtn.innerText = "Dark Mode";
+    }
+
+    if(grandParent.classList.contains("dark-mode")) {
+        const navLinks = document.querySelectorAll('nav-links');
+        navLinks.classList.add('dark-mode')
+    } else {
+        modeBtn.innerText = "Dark Mode";
+    }
+    
+})
+*/
